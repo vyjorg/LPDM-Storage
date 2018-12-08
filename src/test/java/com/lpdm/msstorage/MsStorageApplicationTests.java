ec273f49e8c560b068e9bf21c8c20388145d876a
Kybox <nslr@riseup.net>
2018-12-08 12:14:44 +0100

    Init the microservice

---
+++ src/test/java/com/lpdm/msstorage/MsStorageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lpdm.msstorage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsStorageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
